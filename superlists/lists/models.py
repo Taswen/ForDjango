e8c5a03385920f4ba1d052d155f198ed0f633b6e
Taswen <void_Liberty@hotmail.com>
2021-05-21 19:03:48 +0800

    add items to list

--- superlists/lists/models.py
+++ superlists/lists/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
+class List(models.Model):
+    pass
+
 # Create your models here.
 class Item(models.Model):
     # 没有长度限制的字符串类型字段
     text = models.TextField(default="")
+    list = models.ForeignKey(List,on_delete=models.CASCADE, default=None)
--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -5,7 +5,7 @@ from django.template.loader import render_to_string
 
 from lists.views import home_page   # (2)
 
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your tests here.
 class HomePageTest(TestCase):
@@ -50,17 +50,24 @@ class HomePageTest(TestCase):
     #     self.assertIn('itemey 2',response.content.decode())
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
         '''测试数据库的存与取'''
         first_item = Item()
         first_item.text = "The first (ever) list item"
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Item the second'
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list,list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(),2)
@@ -69,7 +76,9 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
 
         self.assertEqual(first_saved_item.text, "The first (ever) list item")
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, "Item the second")
+        self.assertEqual(second_saved_item.list, list_)
 
 
 
@@ -80,6 +89,7 @@ class ItemModelTest(TestCase):
 class ListViewTest(TestCase):
     
     def test_display_all_items(self):
+        list_ = List.objects.create()
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
 
