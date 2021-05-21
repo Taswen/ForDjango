b540b87925d08de5402812a07f16ff7630087834
Taswen <void_Liberty@hotmail.com>
2021-05-21 14:19:05 +0800

    URLs for forms x2, moved code in views + tests, new URL

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -38,22 +38,7 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response,'home.html')
-
-    def test_can_save_a_POST_request(self):
-        response = self.client.post("/",data ={'item_text':"A new list item"})
-
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, "A new list item")
-
-    def test_redircets_after_POST(self):
-        response = self.client.post("/",data ={'item_text':"A new list item"})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
         
-    def test_only_saves_items_when_necessary(self):
-        self.client.get("/")
-        self.assertEqual(Item.objects.count(), 0)
 
     # def test_display_all_list_items(self):
     #     Item.objects.create(text='itemey 1')
@@ -106,6 +91,20 @@ class ListViewTest(TestCase):
     def test_uses_list_template(self):
         response = self.client.get('/lists/the-only-list-in-the-world/')
         self.assertTemplateUsed(response,'list.html')
+
+
+
+class NewListTest(TestCase):
+    def test_can_save_a_POST_request(self):
+        self.client.post('/lists/new',data={'item_text':'A new list item'})
+
+        self.assertEqual(Item.objects.count(),1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text,'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/lists/new',data={'item_text':'A new list item'})
+        self.assertRedirects(response,'/lists/the-only-list-in-the-world/')    # 测试重定向断言
 
 # (1) Django用来解析URL的function，作用是找到URL应该映射到哪个view
 #     当用“/”调用时，网站的根目录应该找到一个名为home_page的function
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -8,9 +8,9 @@ def home_page(request):
     # 第一个参数是请求，第二个参数要呈现的模板的名称。 
     # Django将在应用程序目录中自动搜索名为templates的文件夹。 
     # 然后，它会根据模板的内容为您构建一个HttpResponse。
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
+    # if request.method == 'POST':
+    #     Item.objects.create(text=request.POST['item_text'])
+    #     return redirect('/lists/the-only-list-in-the-world/')
     return render(request,'home.html')
     # return HttpResponse("<html><title>To-Do lists</title></html>")
 
@@ -18,3 +18,9 @@ def home_page(request):
 def view_list(request):
     items = Item.objects.all()
     return render(request,'list.html',{'items':items})
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
+
